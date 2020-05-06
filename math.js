59701366bc6bc30b7049e22d083b237ec7d7bb29
Corben <example@gmail.com>
2020-05-06 17:27:20 +0300

    add math

---
+++ math.js
@@ -0,0 +1,34 @@
+alert("Do you want to try math?");
+var x = 5;
+var y = 7;
+var z;
+z = (x + y);
+document.write("x = 5");
+document.write("<br/>");
+document.write("y = 7");
+document.write("<br/>");
+document.write("x + y = ");
+document.write(z);
+document.write("<br/>");
+document.write("let d = 6");
+document.write("<br/>");
+var d = 6;
+var r;
+r = (z * d);
+document.write("how much d * z?");
+document.write("<br/>");
+document.write("d * z = ");
+document.write(r);
+document.write("<br/>");
+document.write(r);
+document.write(" > 60?");
+document.write("<br/>");
+if (r > 80) {
+    document.write("true")
+}
+document.write("<br/>");
+document.write("<br/>");
+document.write("<br/>");
+document.write("<br/>");
+document.write("<br/>");
+
